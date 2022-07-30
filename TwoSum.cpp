394c10e4bfe111cb8bebb31d26665c0a20fef1da
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2022-07-31 01:07:52 +0530

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int>twoSum(vector<int>&nums,int target){
+    vector<int>res;
+    unordered_map<int,int>mp;
+    
+    for(int i = 0;i<nums.size();i++){
+        if(mp.find(target-nums[i])!=mp.end()){
+            res.emplace_back(i);
+            res.emplace_back(mp[target-nums[i]]);
+        }
+        mp[nums[i]]=i;
+    }
+    
+    return res;
+}
+};
